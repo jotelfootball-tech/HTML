1d22aea6d249fc5870a1c838330df9ab7dca4487
Joel Taiwo <jotelfootball@gmail.com>
2026-01-15 17:34:22 +0000

    js1

--- joel personal/index.js
+++ joel personal/index.js
@@ -1,2 +1,16 @@
- console.log("hello joel");
+console.log("hello joel");
+
+ alert("I am learning Javascript!");
+  
+
+ let name = "Excellent";
+let age = 25;
+let isLearningJS = true;
+
+console.log(name);
+console.log(age);
+console.log(isLearningJS);
+
+
+
   
