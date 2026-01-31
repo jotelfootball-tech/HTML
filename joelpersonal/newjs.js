3a473ac451360385c8380ce2c3fffba22c2a9b4d
The Joel <jotelfootball@gmail.com>
2026-01-31 13:16:04 +0000

    Thank God

---
+++ joelpersonal/newjs.js
@@ -0,0 +1,13 @@
+// console.log("Hello,world");
+let myname = "Joel";
+console.log (myname);
+
+let isValid =false;
+console.log(typeof isValid);
+
+let x=35.5;
+let y="joy";
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(x+y);
