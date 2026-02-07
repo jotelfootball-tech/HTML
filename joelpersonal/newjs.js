6f0c5d24a5d4f566e26dc47fc7b5fbf44cf70e7e
The Joel <jotelfootball@gmail.com>
2026-02-07 13:06:48 +0000

    i am learning JS

--- joelpersonal/newjs.js
+++ joelpersonal/newjs.js
@@ -11,3 +11,50 @@ let y="joy";
 console.log(typeof x);
 console.log(typeof y);
 console.log(x+y);
+
+// forumla for velocity
+let v; let d; let t;
+ d=10;
+ t=2;
+ v=d/t;
+
+ console.log("The velocity is:" +v);
+
+  // for converting C to F 
+  let F; let C;
+  C=100;
+  F=(C*9/5)+32;
+  console.log("The Fahrenheit is:" +F);
+
+  // for gravitational force formula
+  let f; let G; let m1; let m2; let r;
+  G=6.674*10**-11;
+  m1=2;
+  m2=35;
+  r=12;
+  f=G*(m1*m2)/Math.pow(r,2);
+  console.log("the value for the gravitational constant is:" +f);
+
+  // assignment operators
+  let a= 10;
+  let b= 5;
+
+  a += b;
+  console.log("Ans:" +a);
+
+  // classwork by web3 tutor
+  let p =10;
+  let q =2;
+
+  // for OR 
+  console.log((p>q || p===10));
+
+  // for AND
+  console.log((p>q && q>p));
+
+  // for not 
+  console.log(!(p===200));
+
+
+
+  
